afc0fd86837fe2c468b3f7834eeb4b374fba6bd7
kalmykov1981 <kalmikovw13@gmail.com>
2019-03-29 15:15:06 +0300

    Initial Commit

---
+++ TestGit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit2
+//
+//  Created by Сергей Калмыков on 3/29/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
